9f7352fdfc707a9a9696b0058de8acdfb00f1754
wook_dragon <wook_dragon@kakao.com>
2019-11-28 19:27:51 +0900

    reset

--- CampingEasy/src/kr/or/bit/action/Action.java
+++
@@ -1,9 +0,0 @@
-package kr.or.bit.action;
-
-import javax.naming.NamingException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws NamingException;
-}
